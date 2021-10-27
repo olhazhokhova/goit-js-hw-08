51b28b9552bb45b071b4a87dba9078ddacf6eb5f
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-10-27 18:28:24 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+const player = new Player(document.querySelector('iframe'));
+
+player.on('timeupdate', throttle(setVideoplayerCurrentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
+  console.log(error);
+});
+
+function setVideoplayerCurrentTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
